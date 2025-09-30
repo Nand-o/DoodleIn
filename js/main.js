cd355646f173ade688a64101a6774e090fbb9b13
Nando <ernandofebrian6@gmail.com>
2025-09-30 14:09:55 +0700

    Adding JS to .logo when get click go to home

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@
 
 const header = document.querySelector('header');
 const aboutSection = document.querySelector('#about-section');
+const homeBtn = document.querySelector('.logo');
 
 const observerOptions = {
     root: null,
     threshold: 0.3 
 };
+
+homeBtn.addEventListener('click', () => {
+    window.location.href = './index.html';
+});
 
 const observer = new IntersectionObserver(function(entries, observer) {
     entries.forEach(entry => {
